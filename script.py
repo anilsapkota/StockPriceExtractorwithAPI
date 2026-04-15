0da3ba1b90d34828a63b67f7fdcf7979d093901f
Anil Sapkota <muzicmaniac2015@gmail.com>
2026-04-14 19:06:12 -0500

    basic request for data check

--- script.py
+++ script.py
@@ -8,17 +8,18 @@ load_dotenv()
 POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
 print("API Key:", POLYGON_API_KEY)
 
-
+limit = 100
 tickers = []
-url = f'https://api.massive.com/v3/reference/tickers?market=stocks&active=true&order=asc&limit=100&sort=ticker&apiKey={POLYGON_API_KEY}'
+url = f'https://api.massive.com/v3/reference/tickers?market=stocks&active=true&order=asc&limit={limit}&sort=ticker&apiKey={POLYGON_API_KEY}'
 
 
 
 response = requests.get(url)
 data = response.json()
 print(data.keys())
+print(data['next_url'])
 
 for ticker in data['results']:
     tickers.append(ticker)
 
-print((tickers))
+print(len(tickers))
