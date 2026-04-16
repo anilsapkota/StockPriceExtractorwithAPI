faeb5793410f772727bb9dbe5adc1f16c64ba2c8
Anil Sapkota <muzicmaniac2015@gmail.com>
2026-04-16 13:21:19 -0500

    converted into a function for scheduler

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import requests
 import openai
 import os
 import time
-import csv 
+import csv
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,48 +11,49 @@ POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
 print("API Key:", POLYGON_API_KEY)
 
 limit = 1000
-tickers = []
-url = f'https://api.massive.com/v3/reference/tickers?market=stocks&active=true&order=asc&limit={limit}&sort=ticker&apiKey={POLYGON_API_KEY}'
 
 
+def run_stock_job():
+    tickers = []
+    url = f'https://api.massive.com/v3/reference/tickers?market=stocks&active=true&order=asc&limit={limit}&sort=ticker&apiKey={POLYGON_API_KEY}'
 
-response = requests.get(url)
-data = response.json()
-print(data.keys())
-print(data['next_url'])
-
-for ticker in data['results']:
-    tickers.append(ticker)
-
-
-while 'next_url' in data:
-    time.sleep(15)  # free tier allows ~5 requests/min
-    print("Fetching more results...")
-    response = requests.get(data['next_url'] + f'&apiKey={POLYGON_API_KEY}')
+    response = requests.get(url)
     data = response.json()
-
-    if 'results' not in data:
-        print("Unexpected response:", data)
-        break
+    print(data.keys())
+    print(data['next_url'])
 
     for ticker in data['results']:
         tickers.append(ticker)
 
+    while 'next_url' in data:
+        time.sleep(15)  # free tier allows ~5 requests/min
+        print("Fetching more results...")
+        response = requests.get(data['next_url'] + f'&apiKey={POLYGON_API_KEY}')
+        data = response.json()
 
-example_ticker = tickers[0]
-print("Example Ticker:",example_ticker)
+        if 'results' not in data:
+            print("Unexpected response:", data)
+            break
 
-fieldnames = list(example_ticker.keys())
-print(fieldnames)
+        for ticker in data['results']:
+            tickers.append(ticker)
 
-output_csv = 'tickers.csv'
-with open(output_csv,mode = 'w',newline = '',encoding='utf-8') as f:
-    writer = csv.DictWriter(f,fieldnames=fieldnames)
-    writer.writeheader()
+    example_ticker = tickers[0]
+    print("Example Ticker:", example_ticker)
 
-    for t in tickers:
-        row = {key:t.get(key,'') for key in fieldnames}
-        writer.writerow(row)
+    fieldnames = list(example_ticker.keys())
+    print(fieldnames)
 
-print(f'Wrote {len(tickers)} row to {output_csv}')
+    output_csv = 'tickers.csv'
+    with open(output_csv, mode='w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for t in tickers:
+            row = {key: t.get(key, '') for key in fieldnames}
+            writer.writerow(row)
+
+    print(f'Wrote {len(tickers)} rows to {output_csv}')
+
+
 
